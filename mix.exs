1b9a351b7f565568fd3391f7d9cb6b5d2fa56c1c
Duff OMelia <duff@omelia.org>
2015-08-27 06:58:41 -0400

    11_currency_calculator example using external API

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule HoganExercisesElixir.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,10 @@ defmodule HoganExercisesElixir.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.7.2"},
+      {:poison, "~> 1.5"}
+    ]
   end
+
 end
