4d05e1481accc50b400a673a4f99f7f42b798f07
Duff OMelia <duff@omelia.org>
2015-09-14 07:37:11 -0400

    Simplify 21_numbers_to_names

--- lib/21_numbers_to_names/a.ex
+++ lib/21_numbers_to_names/a.ex
@@ -2,24 +2,11 @@ defmodule NumbersToNames.A do
 
   import InputRetriever
 
-  @months %{
-    1  => "January",
-    2  => "February",
-    3  => "March",
-    4  => "April",
-    5  => "May",
-    6  => "June",
-    7  => "July",
-    8  => "August",
-    9  => "September",
-    10 => "October",
-    11 => "November",
-    12 => "December"
-  }
+  @months ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
   def go do
     number = retrieve_integer("Please enter the number of the month: ", 1..12)
-    IO.puts "The name of the month is #{@months[number]}."
+    IO.puts "The name of the month is #{@months |> Enum.at(number - 1)}."
   end
 
 end
